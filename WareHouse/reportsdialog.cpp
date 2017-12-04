a997eb12443564698f85965108a25e8645199795
Shahzaib Akash <shahzaibakash@gmail.com>
2017-12-04 23:05:24 +0500

    Referenced Database Created an enum for report types created basic format of the functionality added setupModels function Added default dates

--- WareHouse/reportsdialog.cpp
+++ WareHouse/reportsdialog.cpp
@@ -1,11 +1,28 @@
 #include "reportsdialog.h"
 #include "ui_reportsdialog.h"
 
-ReportsDialog::ReportsDialog(QWidget *parent) :
+ReportsDialog::ReportsDialog(qint8 reportType, QSqlDatabase *database, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ReportsDialog)
 {
     ui->setupUi(this);
+    report_type = reportType;
+    db = database;
+
+    setupModel();
+}
+
+void ReportsDialog::setupModel()
+{
+    if (report_type == SALES_VENDORWISE){
+        setupSalesVendorWiseModel();
+    }
+}
+
+void ReportsDialog::setupSalesVendorWiseModel()
+{
+    QSqlQuery q(*db);
+    q.prepare()
 }
 
 ReportsDialog::~ReportsDialog()
